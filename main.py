39e694384b31ca1468ef81c2b124f8e287643665
LiadUziel <84779407+LiadUziel@users.noreply.github.com>
2021-09-18 21:09:36 +0300

    Remove duplicate detections with NMS

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
+import numpy as np
 
-thresh = 0.60  # Threshold to detect object (Accuracy percentages)
+thresh = 0.55  # Threshold to detect object (Accuracy percentages)
+nms_threshold = 0.2  # Non Maximum Suppression - if 1 NMS will not work
 
 capture_video = cv2.VideoCapture(1)  # 1 for DroidCam
 capture_video.set(3, 640)
@@ -26,25 +28,33 @@ while True:  # if image - remove this
     sucess, img = capture_video.read()  # if image - remove this
 
     class_Ids, confs, bbox = net.detect(img, confThreshold=thresh)
-    print(class_Ids, bbox)
+    # print(class_Ids, bbox)
 
-    if len(class_Ids) != 0:
-        for class_ID, confidence, box in zip(class_Ids.flatten(), confs.flatten(), bbox):
-            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+    # Cast to list
+    bbox = list(bbox)
+    confs = list(np.array(confs).reshape(1, -1)[0])
+    confs = list(map(float, confs))
 
-            # Object name
-            cv2.putText(img, class_names[class_ID - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1,
-                        (0, 255, 0), 2)
+    indices = cv2.dnn.NMSBoxes(bbox, confs, thresh, nms_threshold)
 
-            # Accuracy percentages
-            cv2.putText(img, str(round(confidence * 100, 2)) + '%', (box[0] + 200, box[1] + 30),
-                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    for i in indices:
+        i = i[0]  # ignore extra bracket
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+
+        # Draw rectangle
+        cv2.rectangle(img, (x, y), (x+w, h+y), color=(0, 255, 0), thickness=2)
+        # Object name
+        cv2.putText(img, class_names[class_Ids[i][0] - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1,
+                    (0, 255, 0), 2)
+        # Accuracy percentages
+        cv2.putText(img, str(round(confs[i] * 100, 2)) + '%', (box[0] + 200, box[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Object Detection", img)
 
-    cv2.waitKey(1)  # cv2.waitKey(0)
+    cv2.waitKey(1)  # cv2.waitKey(0) # for image 0, for video 1.
 
     if cv2.getWindowProperty("Object Detection", cv2.WND_PROP_VISIBLE) < 1:
         break
-
     # End of loop
